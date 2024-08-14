4134b677e530dda2656303218cc71ca98a83fa7e
7aGiven <fredrickareichert@gmail.com>
2024-08-14 22:12:04 +0800

    inital

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # PhiLogin
 SessionToken By Web or QRcode
+
+# Introduction
+A HTTP Server for API to Login Phigros By Taptap
+
+# API
+The {} is Placeholder, Please remove it.
+```
+get("/login").body("{device_id}")
+{"device_code":"?","qrcode_url":"?"}
+```
+device_id: Redmi Note 5, Nova 6(5G), etc
+
+device_code: use at get_token
+
+qrcode_url: Give the url to user, open it on Taptap APP or Web Browser.
+```
+get("/token/{device_code}").body("{device_id}")
+{"code":-1}
+{"sessionToken":"?"}
+```
+return code:-1 until user complete qrcode_url
+
+then return sessionToken
---
+++ src/main.rs
@@ -0,0 +1,106 @@
+use tokio::runtime;
+use reqwest::Client;
+use axum::routing;
+use axum::extract::{Path, State};
+use hmac::Mac;
+use base64::Engine;
+use rand::{RngCore, SeedableRng};
+
+#[derive(Clone)]
+struct Share {
+    client: Client,
+    tap: reqwest::header::HeaderMap,
+    phi: reqwest::header::HeaderMap
+}
+
+#[derive(serde::Deserialize)]
+struct Wrap<T> {
+    success: bool,
+    data: T
+}
+
+#[derive(serde::Deserialize)]
+struct Device {
+    device_code: String,
+    qrcode_url: String
+}
+
+#[derive(serde::Deserialize)]
+struct Token {
+    kid: String,
+    mac_key: String
+}
+
+#[derive(serde::Deserialize)]
+struct Account {
+    openid: String,
+    unionid: String
+}
+
+fn mac(token: &Token) -> String {
+    let ts: u64 = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
+    let nonce: u32 = rand::rngs::SmallRng::seed_from_u64(ts).next_u32();
+    let input: String = format!("{}\n{}\nGET\n/account/basic-info/v1?client_id=rAK3FfdieFob2Nn8Am\nopen.tapapis.cn\n443\n\n", ts, nonce);
+    let mut mac = hmac::Hmac::<sha1::Sha1>::new_from_slice(token.mac_key.as_bytes()).unwrap();
+    mac.update(input.as_bytes());
+    let mac: String = base64::prelude::BASE64_STANDARD.encode(mac.finalize().into_bytes());
+    format!("MAC id=\"{}\",ts=\"{}\",nonce=\"{}\",mac=\"{}\"", token.kid, ts, nonce, mac)
+}
+
+async fn login(State(state): State<Share>, device_id: String) -> String {
+    let json: Wrap<Device> = state.client.post("https://www.taptap.cn/oauth2/v1/device/code")
+        .headers(state.tap.clone())
+        .body(format!("client_id=rAK3FfdieFob2Nn8Am&response_type=device_code&scope=basic_info&version=1.2.0&platform=unity&info=%7b%22device_id%22%3a%22{}%22%7d", device_id))
+        .send().await.unwrap().json::<Wrap<Device>>().await.unwrap();
+    format!("{{\"device_code\":\"{}\",\"qrcode_url\":\"{}\"}}", json.data.device_code, json.data.qrcode_url)
+}
+
+async fn token(State(state): State<Share>, Path(device_code): Path<String>, device_id: String) -> String {
+    let json = state.client.post("https://www.taptap.com/oauth2/v1/token")
+        .headers(state.tap.clone())
+        .body(format!("grant_type=device_token&client_id=rAK3FfdieFob2Nn8Am&secret_type=hmac-sha-1&code={}&version=1.0&platform=unity&info=%7b%22device_id%22%3a%22{}%22%7d", device_code, device_id))
+        .send().await.unwrap().json::<Wrap<serde_json::Value>>().await.unwrap();
+    if json.success == false {
+        return serde_json::to_string(&json.data).unwrap();
+    }
+    let token: Token = serde_json::from_value(json.data).unwrap();
+    let account: Account = state.client.get("https://open.tapapis.cn/account/basic-info/v1?client_id=rAK3FfdieFob2Nn8Am")
+        .headers(state.tap.clone())
+        .header("Authorization", mac(&token))
+        .send().await.unwrap().json::<Wrap<Account>>().await.unwrap().data;
+    state.client.post("https://rak3ffdi.cloud.tds1.tapapis.cn/1.1/users")
+        .headers(state.phi.clone())
+        .body(format!("{{\"authData\":{{\"taptap\":{{\"kid\":\"{}\",\"access_token\":\"{}\",\"token_type\":\"mac\",\"mac_key\":\"{}\",\"mac_algorithm\":\"hmac-sha-1\",\"openid\":\"{}\",\"unionid\":\"{}\"}}}}}}", token.kid, token.kid, token.mac_key, account.openid, account.unionid))
+        .send().await.unwrap().text().await.unwrap()
+}
+
+async fn start_server() {
+    let mut share = Share {
+        client: reqwest::ClientBuilder::new().http1_title_case_headers()
+            .user_agent("TapTapUnitySDK/1.0 UnityPlayer/2021.3.40f1c1")
+            .build().unwrap(),
+        tap: reqwest::header::HeaderMap::new(),
+        phi: reqwest::header::HeaderMap::new()
+    };
+    share.tap.append("Content-Type", "application/x-www-form-urlencoded".parse().unwrap());
+    share.tap.append("User-Agent", "TapTapAndroidSDK/3.16.5".parse().unwrap());
+    share.phi.append("User-Agent", "LeanCloud-CSharp-SDK/1.0.3".parse().unwrap());
+    share.phi.append("X-LC-Id", "rAK3FfdieFob2Nn8Am".parse().unwrap());
+    share.phi.append("X-LC-Key", "Qr9AEqtuoSVS3zeD6iVbM4ZC0AtkJcQ89tywVyi0".parse().unwrap());
+    share.phi.append("Content-Type", "application/json".parse().unwrap());
+    let app = axum::Router::new()
+        .route("/login", routing::post(login))
+        .route("/token/:device_code", routing::post(token))
+        .layer(tower_http::cors::CorsLayer::permissive())
+        .with_state(share);
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+fn main() {
+    let rt: runtime::Runtime = runtime::Builder::new_current_thread()
+        .enable_io()
+        .enable_time()
+        .build().unwrap();
+    rt.block_on(start_server());
+}
